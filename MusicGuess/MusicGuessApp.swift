042d1fd43166d1c25691d1670b040a75e47b2ca2
princetonwong <princetonwong2@gmail.com>
2022-07-13 16:02:30 +0800

    Initial Commit

---
+++ MusicGuess/MusicGuessApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicGuessApp.swift
+//  MusicGuess
+//
+//  Created by Princeton Wong on 13/7/2022.
+//
+
+import SwiftUI
+
+@main
+struct MusicGuessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
